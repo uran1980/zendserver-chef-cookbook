7831afcd4f6e58a838f27e7f8fa88a33b294cdcd
Lorenzo Fontana <fontanalorenzo@me.com>
2013-09-26 08:01:11 +0200

    Added license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'zendserver'
 maintainer       'Fontana Lorenzo'
 maintainer_email 'fontanalorenzo@me.com'
-license          ''
+license          'MIT'
 description      'Installs/Configures zendserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.2'
